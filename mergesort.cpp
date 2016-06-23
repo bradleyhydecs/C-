248e6ba715c653e02bc1d9806529db308e08ebe0
Bradley Hyde <bradleyhydecs@users.noreply.github.com>
2016-06-24 02:13:24 +1000

    Add files via upload

--- mergesort.cpp
+++ mergesort.cpp
@@ -69,7 +69,5 @@ int main()
 
 	mergesort(arr,0,9);
 
-	printf("[%d %d %d %d %d %d %d %d %d %d]", arr[0], arr[1], arr[2], arr[3], 
-											  arr[4], arr[5], arr[6], arr[7], 
-											  arr[8], arr[9] );
+	printf("[%d %d %d %d %d %d %d %d %d %d]", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9] );
 }
