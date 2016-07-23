aa827d12efa582f1526eacdbcbf80f53d4efa17e
Bradley Hyde <bradleyhydecs@users.noreply.github.com>
2016-07-23 10:38:26 +1000

    Add files via upload

---
+++ tree_lowest_common_ancestor.cpp
@@ -0,0 +1,9 @@
+node * lca(node * root, int v1,int v2)
+{
+    if (v1 < root->data && v2 < root->data)
+        return lca(root->left, v1, v2);
+    if (v1 > root->data && v2 > root->data)
+        return lca(root->right, v1, v2);
+    
+    return root;   
+}
