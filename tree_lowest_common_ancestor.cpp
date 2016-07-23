7839607d484f4cdcd72216d521ab6528b400036f
Bradley Hyde <bradleyhydecs@users.noreply.github.com>
2016-07-23 10:45:23 +1000

    Update tree_lowest_common_ancestor.cpp

--- tree_lowest_common_ancestor.cpp
+++ tree_lowest_common_ancestor.cpp
@@ -1,3 +1,11 @@
+// bradley hyde
+// bradleyhydecs@gmail.com
+// z5063156@zmail.unsw.edu.au
+
+// we can simply navigate the tree until we find the 
+// first point in which our node value is not greater 
+// than or less than both targets
+
 node * lca(node * root, int v1,int v2)
 {
     if (v1 < root->data && v2 < root->data)
